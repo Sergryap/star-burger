709df0055f99052d3f735a01063ffdaff7418b78
sergryap <rs1180@mail.ru>
2022-12-22 21:50:40 +0500

    Modified deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -12,6 +12,10 @@ cd /
 cd /opt/star-burger
 git pull
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+if ! [ -e venv ]
+then
+python3 -m venv venv
+fi
 source venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
